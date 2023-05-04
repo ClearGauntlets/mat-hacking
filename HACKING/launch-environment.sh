04634b9cf0eb2c428a83b42c6c2a872d62eef8eb
Will Nilges <wdn5796@rit.edu>
2023-05-04 15:09:40 -0400

    Add HACKING dir from ImagineRIT2022
    Not all this info is accurate or useful

---
+++ HACKING/launch-environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+IMAGINE_DIR=$(pwd)
+mkdir -p "$IMAGINE_DIR"
+podman run --rm -it                             \
+    --name=esp-idf-dev                          \
+    -v "$IMAGINE_DIR":/workspace                \
+    --device=/dev/ttyUSB0:rwm                   \
+    --group-add keep-groups                     \
+    --annotation io.crun.keep_original_groups=1 \
+    --annotation run.oci.keep_original_groups=1 \
+    --security-opt label=disable \
+    esp-idf-dev
+
+# To open a shell, run: podman exec -it esp-idf-dev bash
+# To add device, insert a device mapping:
+#    --device=/dev/ttyUSB0    \
