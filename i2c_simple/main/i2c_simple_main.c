940cb382e5bf0375d456151e4c76602ff407865f
Will Nilges <wdn5796@rit.edu>
2023-05-11 14:15:58 -0400

    Dude I am GETTING INFORMATION

--- i2c_simple/main/i2c_simple_main.c
+++ i2c_simple/main/i2c_simple_main.c
@@ -20,7 +20,13 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 
-static const char *TAG = "i2c-simple-example";
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+static const char *TAG = "MAT Hacking";
 
 #define MY_SLAVE_ADDR 0x0A
 
@@ -54,6 +60,32 @@ static const char *TAG = "i2c-simple-example";
 #define FLASH_CMD_WRITE_EN      0x06
 #define FLASH_CMD_ERASE_SECTOR  0xD8
 #define FLASH_CMD_WAKEUP        0xAB
+#define LHBL_ENABLE_UART        0xBC
+
+/**
+ * This is an example which echos any data it receives on configured UART back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: configured UART
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below (See Kconfig)
+ */
+
+#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
+#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM      (CONFIG_EXAMPLE_UART_PORT_NUM)
+#define ECHO_UART_BAUD_RATE     (CONFIG_EXAMPLE_UART_BAUD_RATE)
+#define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
+
+#define BUF_SIZE (8192)
+
+
 
 /**
  * @brief Read a sequence of bytes from a MPU9250 sensor registers
@@ -79,6 +111,11 @@ static esp_err_t mpu9250_register_write_byte(uint8_t reg_addr, uint8_t data)
 static esp_err_t lighthouse_deck_run_command(uint8_t command, uint8_t *data, size_t len)
 {
     return i2c_master_write_read_device(I2C_MASTER_NUM, LIGHTHOUSE_DECK_ADDR, &command, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+}
+
+static esp_err_t lighthouse_deck_run_command_no_return(uint8_t command)
+{
+    return i2c_master_write_to_device(I2C_MASTER_NUM, LIGHTHOUSE_DECK_ADDR, &command, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 }
 
 /*
@@ -147,18 +184,70 @@ void app_main(void)
     }
     printf("Done.\n");
 
-    int ret;
+    printf("Configuring UART...\n");
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 230400,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_REF_TICK,
+    };
+    int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+
     ESP_LOGI(TAG, "Get firmware version...");
     //ESP_ERROR_CHECK(lighthouse_deck_get_version(data, 1));
     ESP_ERROR_CHECK_WITHOUT_ABORT(lighthouse_deck_run_command(LHBL_GET_VERSION, data, 1));
     ESP_LOGI(TAG, "Lighthouse Deck Bootloader Version = %X", data[0]);
-    //ret = lighthouse_deck_run_command(LHBL_GET_VERSION, data_but_again, 1);
-    //ESP_LOGI(TAG, "Lighthouse Deck Bootloader Version = %X", data_but_again[0]);
-    //ESP_LOGI(TAG, "Ret = %d", ret);
 
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
+    ESP_ERROR_CHECK_WITHOUT_ABORT(lighthouse_deck_run_command_no_return(LHBL_BOOT_TO_FW));
+    ESP_LOGI(TAG, "Deck booting...");
 
-/*
+    /*
+    ESP_LOGI(TAG, "Attempting boot in 5 seconds...");
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    // Configure a temporary buffer for the incoming data
+    char break_condition_and_boot[5] = { 0x00, 0x00, 0x00, 0xBC, 0x00 };
+    //char boot[1] = { 0xBC }; 
+    //uart_write_bytes(ECHO_UART_PORT_NUM, break_condition, 3);
+    uart_write_bytes(ECHO_UART_PORT_NUM, break_condition_and_boot, 5);
+    */
+
+    uint8_t *output = (uint8_t *) malloc(BUF_SIZE);
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, output, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        // Write data back to the UART
+        //uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
+        
+        //uart_write_bytes(ECHO_UART_PORT_NUM, "Hello", 5);
+        //vTaskDelay(5000 / portTICK_PERIOD_MS);
+        
+        if (len) {
+            output[len] = '\0';
+            ESP_LOGI(TAG, "Recv str: %s", (char *) output);
+        }
+        
+    }
+
+    /*
+    ESP_LOGI(TAG, "Lighthouse Deck Enable UART");
+    ESP_ERROR_CHECK_WITHOUT_ABORT(lighthouse_deck_run_command(LHBL_ENABLE_UART, data, 1));
+    ESP_LOGI(TAG, "Response: %s", data);
+
     ESP_LOGI(TAG, "Attempting boot in 5 seconds...");
     vTaskDelay(5000 / portTICK_PERIOD_MS);
 
